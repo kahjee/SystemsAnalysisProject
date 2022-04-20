a8e9ebbe84126dafe18aca3d352607b7a3d007fd
JCJ16 <89191944+JCJ8914@users.noreply.github.com>
2022-04-21 01:34:07 +0800

    Widgets Added

--- project.py
+++ project.py
@@ -28,7 +28,7 @@ RightFrame3.grid(row=3, column=0)
 BottomFrame = Frame(MainFrame, bd=10, width=1350, height=150, padx=2, relief=RIDGE)
 BottomFrame.pack(side=BOTTOM)
 
-#=======================================WIDGETS==================================================
+#=======================================LEFT WIDGETS==================================================
 root.lblCusID = Label(LeftFrame, font=('arial', 12,'bold'), text="Customer No:", padx=1)
 root.lblCusID.grid(row=0, column=0, sticky =W)
 root.txtCusID =Entry(LeftFrame, font=('arial',12,'bold') ,width =18)
@@ -69,8 +69,55 @@ root.lblCheckout.grid(row=7, column=0, sticky =W)
 root.txtCheckout =Entry(LeftFrame, font=('arial',12,'bold') ,width =18)
 root.txtCheckout.grid(row=7, column=1, pady=3, padx=20)
 
-#=======================================WIDGETS==================================================
+#=======================================RIGHT WIDGETS==================================================
 root.lblLabel = Label(RightFrame1, font=('arial', 9,'bold'), padx=6, pady=10, text="Customer No\tFirstname\t Surname \t Contact No \t Address \t Room Num \tCheck In Date\t Check Out Date")
 root.lblLabel.grid(row=0, column=0, columnspan=17)
+
+scrollbar= Scrollbar(RightFrame2)
+scrollbar.grid(row=0, column=0,sticky='ns')
+lstReso = Listbox(RightFrame2, width=103, height=14, font=('arial', 9, 'bold'), yscrollcommand= scrollbar.set)
+lstReso.bind('<<ListboxSelct>>')
+lstReso.grid(row=0, column=0, padx=7, sticky= 'nsew')
+scrollbar.config(command = lstReso.xview)
+#=======================================RIGHT WIDGETS==================================================
+root.lblDays = Label(RightFrame3, font=('arial', 12,'bold'), text="No. of Days:", padx=2, pady=2)
+root.lblDays.grid(row=0, column=0, sticky =W)
+root.txtDays =Entry(RightFrame3, font=('arial',12,'bold') ,width =76)
+root.txtDays.grid(row=0, column=1, pady=3, padx=20)
+
+root.lblPaid = Label(RightFrame3, font=('arial', 12,'bold'), text="Amount Paid:", padx=2, pady=2)
+root.lblPaid.grid(row=1, column=0, sticky =W)
+root.txtPaid =Entry(RightFrame3, font=('arial',12,'bold') ,width =76)
+root.txtPaid.grid(row=1, column=1, pady=3, padx=20)
+
+root.lblTotal = Label(RightFrame3, font=('arial', 12,'bold'), text="Total Cost:", padx=2, pady=2)
+root.lblTotal.grid(row=2, column=0, sticky =W)
+root.txtTotal =Entry(RightFrame3, font=('arial',12,'bold') ,width =76)
+root.txtTotal.grid(row=2, column=1, pady=3, padx=20)
+#=======================================WIDGET BUTTONS==================================================
+
+root.btnTotalandAddData = Button(BottomFrame, bd=4, font=('arial', 16,'bold'),
+width=13, height=2, text='AddNew/Total').grid(row=0, column=0, padx =4,  pady=1)
+
+root.btnDisplay = Button(BottomFrame, bd=4, font=('arial', 16,'bold'),
+width=13, height=2, text='Display').grid(row=0, column=1, padx =4,  pady=1)
+
+root.btnUpdate = Button(BottomFrame, bd=4, font=('arial', 16,'bold'),
+width=13, height=2, text='Update').grid(row=0, column=2, padx =4,  pady=1)
+
+root.btnDelete = Button(BottomFrame, bd=4, font=('arial', 16,'bold'),
+width=13, height=2, text='Delete').grid(row=0, column=3, padx =4,  pady=1)
+
+root.btnSearch = Button(BottomFrame, bd=4, font=('arial', 16,'bold'),
+width=13, height=2, text='Search').grid(row=0, column=4, padx =4,  pady=1)
+
+root.btnReset = Button(BottomFrame, bd=4, font=('arial', 16,'bold'),
+width=13, height=2, text='Reset').grid(row=0, column=5, padx =4,  pady=1)
+
+root.btnExit = Button(BottomFrame, bd=4, font=('arial', 16,'bold'),
+width=13, height=2, text='Exit').grid(row=0, column=6, padx =4,  pady=1)
+
+
+
 
 root.mainloop()
